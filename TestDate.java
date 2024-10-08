7eebd3ff11a2f8b731e7055b8c12c20112a4de24
Nikhil N <88040968+Nikhil9863@users.noreply.github.com>
2024-10-08 17:44:39 +0530

    Add files via upload

---
+++ TestDate.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class TestDate {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Nikhil");
+        System.out.println(new Date());
+    }
+    
+}
